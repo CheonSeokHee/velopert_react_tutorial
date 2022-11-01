a88516661bf046ec2289eb90ce9644524075710b
cheonSeokHee <csh3064@naver.com>
2022-11-01 13:57:01 +0900

    velopert api axios 연습

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,9 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 
 //api 호출 예제
 //axios 예제 api 연동의 기본
+
+//useState 의 setState 함수를 여러번 사용하지 않아도 된다는점,
+//리듀서로 로직을 분리했으니 다른곳에서도 쉽게 재사용을 할 수 있다는 점
+/*
+function reducer(state, action) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null
+            };
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            };
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error
+            };
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+}
+*/
+
 //요청에 대한 상태를 관리 (총 3가지 상태 관리)
 // 요청의 결과
 // 로딩 상태
@@ -34,8 +64,8 @@ function Users() {
         fetchUsers();
     }, []);
 
-    if (loading) return <div>로딩중 입니다..</div>;
-    if (error) return <div>에러가 발생했습니다 요청을 다시 확인 해주세요</div>;
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>에러가 발생했습니다</div>;
     if (!users) return null;
     return (
         <>
